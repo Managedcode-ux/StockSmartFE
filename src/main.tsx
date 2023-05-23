26984a8a3dc1023e7c77089e66d20ef3d807285f
Managedcode-ux <71963227+Managedcode-ux@users.noreply.github.com>
2023-05-23 17:23:04 +0530

    BATMAN

---
+++ src/main.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { Login_SignUp_Comp } from './components/Login-Signup-popup/Login-signup-popup'
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <Login_SignUp_Comp/>
+  </React.StrictMode>,
+)
